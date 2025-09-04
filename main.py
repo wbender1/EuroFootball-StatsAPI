f4d41211a08385d84e9af4f90b07fcb90b942812
William Bender <w.bender00@gmail.com>
2025-09-04 17:11:09 -0400

    Print Premier League Standings

--- main.py
+++ main.py
@@ -20,5 +20,20 @@ headers = {
 # Send GET request and receive response
 response = requests.get(url, headers=headers, params=params)
 
-# Print response text
-print(response.text)
+# Parse JSON into Python Dictionary
+data = response.json()
+
+league = data['response'][0]['league']['name']
+country = data['response'][0]['league']['country']
+season = data['response'][0]['league']['season']
+teams = []
+for team in data['response'][0]['league']['standings'][0]:
+    name = team['team']['name']
+    points = team['points']
+    rank = team['rank']
+    teams.append(f'{rank} - {name} : {points}')
+
+output = f'The standings for the {season} {country} {league} are: '
+print(output)
+for team in teams:
+    print(team)
